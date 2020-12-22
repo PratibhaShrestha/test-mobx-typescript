a2a6bd3af9b55013daf84e9b5f0bf0e1478b589b
PratibhaShrestha <pateebha@gmail.com>
2020-12-22 12:17:25 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {observer} from 'mobx-react';
 import './App.css';
+import MainDiv from "./Components/MainDiv"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+@observer
+class App extends Component<{}> {
+  render(){
+    return (
+      <div className="App">
+        <MainDiv />
+      </div>
+    );
+
+  }
 }
 
 export default App;
